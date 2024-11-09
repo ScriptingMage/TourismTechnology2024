fe0a81be3f6d39bce3d3d8873bf80ba917e426d3
Qingue <qingquestatus@outlook.com>
2024-11-09 20:24:55 +0100

    gnome

--- wwbs/src/app/admin/page.tsx
+++ wwbs/src/app/admin/page.tsx
@@ -1,12 +1,11 @@
-import {Component} from "@/app/admin/weeklyOverview";
-import {DailyPieChart} from "@/app/admin/dailyPieChart";
+import WeeklyOverview from "@/app/admin/weeklyOverview";
+import { DailyPieChart } from "@/app/admin/dailyPieChart";
 
-export default function AdminPage()
-{
-    return <div>
-        <Component />
-        <DailyPieChart />
+export default function AdminPage() {
+  return (
+    <div>
+      <WeeklyOverview />
+      <DailyPieChart />
     </div>
+  );
 }
-
-
--- wwbs/src/app/admin/weeklyOverview.tsx
+++ wwbs/src/app/admin/weeklyOverview.tsx
@@ -1,78 +1,101 @@
-"use client"
+"use client";
 
-import { TrendingUp } from "lucide-react"
-import {Bar, BarChart, LabelList, XAxis, YAxis} from "recharts"
+import { TrendingUp } from "lucide-react";
+import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
-} from "@/components/ui/chart"
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import React from "react";
 const chartData = [
-    { month: "Monday", desktop: 7 },
-    { month: "Tuesday", desktop: 8 },
-    { month: "Wednesday", desktop: 6 },
-    { month: "Thursday", desktop: 5 },
-    { month: "Friday", desktop: 5 },
-    { month: "Saturday", desktop: 6 },
-    { month: "Sunday", desktop: 7 },
-]
+  { month: "Monday", total: 30, current: 5 },
+  { month: "Tuesday", total: 30, current: 8 },
+  { month: "Wednesday", total: 26, current: 2 },
+  { month: "Thursday", total: 25, current: 4 },
+  { month: "Friday", total: 15, current: 9 },
+  { month: "Saturday", total: 26, current: 12 },
+  { month: "Sunday", total: 27, current: 11 },
+];
 
 const chartConfig = {
-    desktop: {
-        label: "Bookings",
-        color: "hsl(var(--chart-1))",
-    },
-} satisfies ChartConfig
+  total: {
+    label: "Bookings",
+    color: "#4CAF50",
+  },
+  current: {
+    label: "Bookings",
+    color: "#F44336",
+  },
+} satisfies ChartConfig;
 
-export function Component() {
-    return (
-        <Card>
-            <CardContent>
-                <ChartContainer className="h-64" config={chartConfig}>
-                    <BarChart
-                        accessibilityLayer
-                        data={chartData}
-                        layout="vertical"
-                        margin={{
-                            left: -20,
-                        }}
-                    >
-                        <XAxis type="number" dataKey="desktop" hide />
-                        <YAxis
-                            dataKey="month"
-                            type="category"
-                            tickLine={false}
-                            tickMargin={10}
-                            axisLine={false}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                        />
-                        <ChartTooltip
-                            cursor={false}
-                            content={<ChartTooltipContent hideLabel />}
-                        />
-                        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={5} >
-                            <LabelList
-                                dataKey="desktop"
-                                position="right"
-                                offset={8}
-                                className="fill-foreground"
-                                fontSize={12}
-                            />
-                        </Bar>
-                    </BarChart>
-                </ChartContainer>
-            </CardContent>
-        </Card>
-    )
+export default function WeeklyOverview() {
+  return (
+    <Card>
+      <CardContent>
+        <ChartContainer className="h-64" config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout="vertical"
+            margin={{
+              left: -20,
+            }}
+          >
+            <XAxis type="number" dataKey="total" hide />
+            <YAxis
+              dataKey="month"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar
+              dataKey="total"
+              fill="var(--color-total)"
+              radius={0}
+              stackId="a"
+            >
+              <LabelList
+                dataKey="total"
+                position="right"
+                offset={8}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Bar>
+            <Bar
+              dataKey="current"
+              fill="#F44336"
+              radius={[0, 5, 5, 0]}
+              stackId="a"
+            >
+              <LabelList
+                dataKey="current"
+                position="right"
+                offset={8}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
 }
