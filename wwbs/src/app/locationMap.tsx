a74a875f7c20cda5064af3203092afe35ba02142
Qingue <qingquestatus@outlook.com>
2024-11-09 18:41:10 +0100

    Lines between MapMarkers

--- wwbs/src/app/locationMap.tsx
+++ wwbs/src/app/locationMap.tsx
@@ -1,34 +1,42 @@
 "use client";
 // components/Map.jsx
 import React, { useEffect, useRef, useState } from "react";
-import { Map, Marker } from "react-map-gl";
+import { Map, Marker, Source, Layer } from "react-map-gl";
 import DeckGL from "@deck.gl/react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 import { INITIAL_VIEW_STATE } from "../lib/mapconfig.js";
 
+const markerColors = "red";
+
 export default function LocationAggregatorMap() {
-  const map = useRef();
-  //   const marker = new mapboxgl.Marker({
-  //     color: "#FFFFFF",
-  //     draggable: false,
-  //   }).setLngLat([47.2675, 11.391]);
-
-  //   useEffect(() => {
-  //     marker.addTo(map.current!);
-  //     console.log(map.current);
-  //   }, [map.current]);
-
   const activeMarkers = [
     { longitude: 11.391, latitude: 47.2675 },
-    { longitude: 12.391, latitude: 47.2675 },
-    { longitude: 13.391, latitude: 47.2675 },
-    { longitude: 14.391, latitude: 47.2675 },
-    { longitude: 15.391, latitude: 47.2675 },
+    { longitude: 12.391, latitude: 48.2675 },
+    { longitude: 13.391, latitude: 49.2675 },
+    { longitude: 14.391, latitude: 50.2675 },
+    { longitude: 15.391, latitude: 46.2675 },
   ];
+
+  const lineData = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: activeMarkers.map((location) => [
+            location.longitude,
+            location.latitude,
+          ]),
+        },
+      },
+    ],
+  };
 
   const Markers = activeMarkers.map((marker) => (
     <Marker
+      color={markerColors}
       key={`${marker.longitude}_${marker.latitude}`}
       longitude={marker.longitude}
       latitude={marker.latitude}
@@ -38,11 +46,20 @@ export default function LocationAggregatorMap() {
   return (
     <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
       <Map
-        ref={map as any}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/mapbox/outdoors-v12"
       >
         {Markers}
+        <Source id="line-source" type="geojson" data={lineData}>
+          <Layer
+            id="line-layer"
+            type="line"
+            paint={{
+              "line-color": markerColors, // Line color
+              "line-width": 2, // Line width
+            }}
+          />
+        </Source>
       </Map>
     </DeckGL>
   );
