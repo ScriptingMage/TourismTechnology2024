931875bedde1a141308bf096762f63408f18290e
Fabio Wallner <hi@fabiowallner.com>
2024-11-09 16:43:03 +0100

    add calender & popover from shadcn

---
+++ wwbs/src/app/hiking-trails/book/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto py-20">
+      <h1 className="text-center text-4xl font-bold mb-20">Book a Hiking Trail</h1>
+      </div>
+  )
+  }
