95cfb8cc75c6a7835fc4bcbf757a1bd58a54f70c
Fabio Wallner <hi@fabiowallner.com>
2024-11-09 21:07:18 +0100

    cleanup booking page

--- wwbs/src/app/hiking-trails/book/[id]/page.tsx
+++ wwbs/src/app/hiking-trails/book/[id]/page.tsx
@@ -9,43 +9,6 @@ export default async function Page({ params }: { params: { id: number } }) {
     <div className="container max-w-7xl mx-auto py-20">
       <h1 className="text-4xl font-bold mb-8">Book a Hiking Trail</h1>
       <BookingManager hikingTrail={hikingTrail} />
-      <div className="flex flex-col items-start gap-8">
-        
-
-        {/* <div className="border border-gray-500 p-4 w-auto">
-          <h2 className="text-2xl font-bold">No availability</h2>
-          <p>Unfortunately there is no availability for this hiking trail.</p>
-        </div>
-
-        <HikingStageBookingCard
-            hikingTrailStage={{
-                id: 1,
-                hikingTrailId: 1,
-                position: 1,
-                title: "Start",
-                description: "Short description of the stage",
-                distance: 12.5,
-            }}
-            stageDate={new Date()}
-        />
-
-        
-        <div className="border border-gray-500 p-4 w-auto">
-          <div className="text-sm opacity-50">Date Stage 4</div>
-          <h2 className="text-2xl font-bold">4. Stage: Start - End</h2>
-          <p>Short description of the stage</p>
-          <h3 className="text-xl font-bold border-t border-gray-200 mt-4 pt-4">
-            Overnight Stays:
-          </h3>
-          <div className="grid gap-4 mt-4">
-            <div>
-              <p>No Overnight stay available.</p>
-              <p>You can skip this stage for now.</p>
-              <Button className="mt-4">Skip</Button>
-            </div>
-          </div>
-        </div> */}
-      </div>
     </div>
   );
 }
--- wwbs/src/features/bookings/components/booking-manager.tsx
+++ wwbs/src/features/bookings/components/booking-manager.tsx
@@ -108,6 +108,40 @@ export const BookingManager = ({ hikingTrail }: BookingManagerProps) => {
           );
         }
       })}
+
+      {/* <div className="border border-gray-500 p-4 w-auto">
+          <h2 className="text-2xl font-bold">No availability</h2>
+          <p>Unfortunately there is no availability for this hiking trail.</p>
+        </div>
+
+        <HikingStageBookingCard
+            hikingTrailStage={{
+                id: 1,
+                hikingTrailId: 1,
+                position: 1,
+                title: "Start",
+                description: "Short description of the stage",
+                distance: 12.5,
+            }}
+            stageDate={new Date()}
+        />
+
+        
+        <div className="border border-gray-500 p-4 w-auto">
+          <div className="text-sm opacity-50">Date Stage 4</div>
+          <h2 className="text-2xl font-bold">4. Stage: Start - End</h2>
+          <p>Short description of the stage</p>
+          <h3 className="text-xl font-bold border-t border-gray-200 mt-4 pt-4">
+            Overnight Stays:
+          </h3>
+          <div className="grid gap-4 mt-4">
+            <div>
+              <p>No Overnight stay available.</p>
+              <p>You can skip this stage for now.</p>
+              <Button className="mt-4">Skip</Button>
+            </div>
+          </div>
+        </div> */}
     </div>
   );
 };
