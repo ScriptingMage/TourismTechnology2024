6b34d08dd7b877f923e20f77eb43add0e9ff7344
ScriptMage <scriptmage037@gmail.com>
2024-11-09 20:26:01 +0100

    Admin page update

--- wwbs/src/app/admin/page.tsx
+++ wwbs/src/app/admin/page.tsx
@@ -1,11 +1,20 @@
 import {Component} from "@/app/admin/weeklyOverview";
 import {DailyPieChart} from "@/app/admin/dailyPieChart";
 
-export default function AdminPage()
-{
+export default function AdminPage() {
     return <div>
-        <Component />
-        <DailyPieChart />
+        <div>Your Dashboard</div>
+        <div className="flex relative">
+            <div className="w-1/2 h-full">
+                <DailyPieChart/>
+            </div>
+            <div className="w-1/2 h-full">
+                <Component/>
+            </div>
+
+        </div>
+        <Component/>
+        
     </div>
 }
 
