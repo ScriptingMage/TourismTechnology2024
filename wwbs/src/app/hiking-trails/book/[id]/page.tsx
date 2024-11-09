fc713a8e5fbb7433806a1addb89336ec8005d850
ScriptMage <scriptmage037@gmail.com>
2024-11-09 21:23:50 +0100

    Map in Booking screen.

--- wwbs/src/app/hiking-trails/book/[id]/page.tsx
+++ wwbs/src/app/hiking-trails/book/[id]/page.tsx
@@ -1,13 +1,24 @@
-import { BookingManager } from "@/features/bookings/components/booking-manager";
-import { fetchHikingTrail } from "@/features/hiking-trails/database/actions";
+import {BookingManager} from "@/features/bookings/components/booking-manager";
+import {fetchHikingTrail} from "@/features/hiking-trails/database/actions";
+import LocationAggregatorMap from "@/app/locationMap";
+import {Card} from "@/components/ui/card";
 
-export default async function Page({ params }: { params: { id: number } }) {
-  const hikingTrail = await fetchHikingTrail(params.id);
+export default async function Page({params}: { params: { id: number } }) {
+    const hikingTrail = await fetchHikingTrail(params.id);
 
-  return (
-    <div className="container max-w-7xl mx-auto py-20">
-      <h1 className="text-4xl font-bold mb-8">Book a Hiking Trail</h1>
-      <BookingManager hikingTrail={hikingTrail} />
-    </div>
-  );
+    const showMap = false;
+
+    return (
+        <div className="w-full min-h-screen flex gap-8">
+            <div className="w-1/2 h-screen p-10">
+                <Card className="h-full overflow-hidden [&>*]:!relative">
+                    {showMap ? <LocationAggregatorMap/> : <div className="text-center">Map is disabled</div>}
+                </Card>
+            </div>
+            <div className="w-1/2 h-screen overflow-y-scroll py-10">
+                <h1 className="text-4xl font-bold mb-8">Book a Hiking Trail</h1>
+                <BookingManager hikingTrail={hikingTrail}/>
+            </div>
+        </div>
+    );
 }
