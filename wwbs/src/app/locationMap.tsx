a198c405123b4d09fd74cf588fab54734dccd25a
Qingue <qingquestatus@outlook.com>
2024-11-09 15:53:18 +0100

    Map Shenanigans

--- wwbs/src/app/locationMap.tsx
+++ wwbs/src/app/locationMap.tsx
@@ -1,37 +1,21 @@
 "use client";
 // components/Map.jsx
-
 import React, { useState } from "react";
-
 import Map from "react-map-gl";
 import DeckGL from "@deck.gl/react";
 import "mapbox-gl/dist/mapbox-gl.css";
-
 // import map config
-import {
-    lightingEffect,
-    material,
-    INITIAL_VIEW_STATE,
-    colorRange,
-} from "../lib/mapconfig.js";
+import { INITIAL_VIEW_STATE } from "../lib/mapconfig.js";
 
-const LocationAggregatorMap = () => {
-
-
-    return (
-        <div>
-            <DeckGL
-                effects={[lightingEffect]}
-                initialViewState={INITIAL_VIEW_STATE}
-                controller={true}
-            >
-                <Map
-                    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-                    mapStyle="mapbox://styles/petherem/cl2hdvc6r003114n2jgmmdr24"
-                ></Map>
-            </DeckGL>
-        </div>
-    );
-};
-
-export default LocationAggregatorMap;
+export default function LocationAggregatorMap() {
+  return (
+    <div className="">
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
+        <Map
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+          mapStyle="mapbox://styles/mapbox/outdoors-v12"
+        ></Map>
+      </DeckGL>
+    </div>
+  );
+}
--- wwbs/src/app/page.tsx
+++ wwbs/src/app/page.tsx
@@ -1,15 +1,12 @@
 import LocationAggregatorMap from "@/app/locationMap";
 
 export default function Home() {
-    const token = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+  const token = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
   return (
-      <main className="">
-          {token}
-          <div className="relative min-h-screen">
-              <LocationAggregatorMap/>
-          </div>
-      </main>
+    <main className="">
+      <div className="relative min-h-screen">
+        <LocationAggregatorMap />
+      </div>
+    </main>
   );
 }
-
-
