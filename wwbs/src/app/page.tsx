bfd7d73df5a13301fcf113b3740b8ec3bdf02c65
Qingue <qingquestatus@outlook.com>
2024-11-10 01:21:05 +0100

    Harold smiles upon us

--- wwbs/src/app/page.tsx
+++ wwbs/src/app/page.tsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <main className="flex-grow">
       <div className="py-12">
-        <h1 className="text-4xl font-bold text-center">
-          Welcome to Eagle Wing
-        </h1>
+        <h1 className="text-4xl font-bold text-center">Welcome to GoGnome</h1>
         <p className="text-center">Book Hütten easy</p>
       </div>
     </main>
