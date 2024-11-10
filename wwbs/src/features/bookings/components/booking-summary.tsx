9ead5d4e961920c1e4f8ee908cb0f08025a43804
Fabio Wallner <hi@fabiowallner.com>
2024-11-10 01:54:00 +0100

    save booking

--- wwbs/src/features/bookings/components/booking-summary.tsx
+++ wwbs/src/features/bookings/components/booking-summary.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { Booking, BookingStage } from "../database/types";
 import { formatDuration } from "date-fns";
+import { create } from "domain";
+import { createBooking } from "../database/actions";
 
 interface BookingSummaryProps {
   booking: Booking;
   bookingStages: BookingStage[];
 }
 
-export const BookingSummary = ({ booking, bookingStages }: BookingSummaryProps) => {
+export const BookingSummary = ({
+  booking,
+  bookingStages,
+}: BookingSummaryProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    ("use server");
+    createBooking(booking, bookingStages);
+  };
   return (
     <div className="border border-gray-500 p-4 w-full">
       <h2 className="text-3xl font-bold mb-4">Booking Summary</h2>
@@ -15,7 +25,8 @@ export const BookingSummary = ({ booking, bookingStages }: BookingSummaryProps) 
         <span className="font-bold">Hiking Trail:</span> {booking.name}
       </div>
       <div>
-        <span className="font-bold">Start Date:</span> {booking.startDate.toLocaleDateString()}
+        <span className="font-bold">Start Date:</span>{" "}
+        {booking.startDate.toLocaleDateString()}
       </div>
       <div>
         <span className="font-bold">Hikers:</span> {booking.hikers}
@@ -23,10 +34,12 @@ export const BookingSummary = ({ booking, bookingStages }: BookingSummaryProps) 
       <h3 className="text-2xl font-bold my-4">Booking Stages</h3>
       <div className="flex flex-col gap-4">
         {bookingStages.map((bookingStage, index) => {
-        
           return (
             <div key={bookingStage.id}>
-              <h4 className="text-xl font-bold mb-2">Stage {index+1}: {bookingStage.startDate.toLocaleDateString()} - {bookingStage.endDate.toLocaleDateString()}</h4>
+              <h4 className="text-xl font-bold mb-2">
+                Stage {index + 1}: {bookingStage.startDate.toLocaleDateString()}{" "}
+                - {bookingStage.endDate.toLocaleDateString()}
+              </h4>
               <div>Start: {bookingStage.startDate.toLocaleDateString()}</div>
               <div>Duration: {1} day</div>
               <div>Distance: {0}km</div>
@@ -35,7 +48,11 @@ export const BookingSummary = ({ booking, bookingStages }: BookingSummaryProps) 
           );
         })}
       </div>
-      <Button className="mt-4">Book now</Button>
+      <form onSubmit={handleSubmit}>
+        <Button type="submit" className="mt-4">
+          Book now
+        </Button>
+      </form>
     </div>
   );
 };
--- wwbs/src/features/bookings/database/actions.ts
+++ wwbs/src/features/bookings/database/actions.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/database/db";
 import { Booking, BookingStage } from "./types";
 
 export const createBooking = async (booking: Booking, bookingStages: BookingStage[]) => {
@@ -6,7 +7,26 @@ export const createBooking = async (booking: Booking, bookingStages: BookingStag
         //await new Promise((resolve) => setTimeout(resolve, 1000));
 
         // Simulated database fetch
+        const newBooking = await prisma.booking.create({
+            data: {
+                hikingTrailId: booking.hikingTrailId,
+                startDate: booking.startDate,
+                hikers: booking.hikers,
+            },
+        });
 
+        const newBookingStages = await prisma.bookingStage.createMany({
+            data: bookingStages.map((bookingStage) => {
+                return {
+                    bookingId: newBooking.id,
+                    startDate: bookingStage.startDate,
+                    endDate: bookingStage.endDate,
+                    hikingTrailStageId: bookingStage.hikingTrailStageId,
+                    hikers: bookingStage.hikers,
+                    accommodationId : bookingStage.accommodationId,
+                };
+            }),
+        });
     }
     catch (error) {
         console.error("Error creating booking:", error);
